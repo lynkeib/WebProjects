fda7bff79592bee8ebe9ecd4b69ed4998db21255
Chengyin Liu <liuchengyin1234@126.com>
2019-02-28 15:11:29 -0800

    。

--- pyspark/Yelp/SON_2/Apriori.py
+++ pyspark/Yelp/SON_2/Apriori.py
@@ -1,88 +1,118 @@
+from itertools import combinations
+import itertools
+import operator
+from functools import reduce
+
+
 def create_candidates(candidates_list, length):
     '''
-    :param candidates_list: list[set]
-    :return: list[set]
+    :param candidates_list:
+    :param length:
+    :return:
     '''
-    res = []
-    for item_1 in candidates_list:
-        for item_2 in candidates_list:
-            temp = frozenset(item_1 | item_2)
-            if temp not in res and len(temp) == length:
-                res.append(temp)
-    print(res)
-    return res
+    for comb in combinations(candidates_list, 2):
+        temp = set(comb[0]) | set(comb[1])
+        if len(temp) == length:
+            yield tuple(temp)
 
-def frequent_items(partition, candidates, support):
+
+def frequent_items(dict, k, candidates, support):
     '''
-    :param partition: iterator
-    :param candidates: list[set]
+    :param dict: {business_id: set(user_id)}
+    :param k: int
+    :param candidates: list[list]
     :param support: int
-    :return: list[set]
+    :return: list[list]
     '''
     res = []
-    count = {}
-    for line in partition:
-        for candidate in candidates:
-            if candidate.issubset(line[1]):
-                if tuple(candidate) not in count:
-                    count[tuple(candidate)] = 1
-                else:
-                    count[tuple(candidate)] += 1
-    for key, value in count.items():
-        if value >= support:
-            if set(key) not in res:
-                res.append(frozenset(key))
+    for comb in create_candidates(candidates, k):
+        if set(comb) not in res:
+            temp = reduce(lambda a, b: a & b, (dict[x] for x in comb))
+            if len(temp) >= support:
+                res.append(set(comb))
+    res = [list(comb) for comb in res]
     return res
 
-def Apriori(partition, support, threshold=0):
+
+def convert(line):
+    '''
+    :param line: (key, (values))
+    :return: list[tuple(key, value)]
+    '''
+    res = []
+    for business in line[1]:
+        res.append((business, line[0]))
+    return res
+
+
+def accumulate(l):
+    '''
+    :param l: list[tuple((key, value))]
+    :return:
+    '''
+    it = itertools.groupby(sorted(l), operator.itemgetter(0))
+    res = {}
+    for key, subiter in it:
+        res[key] = {item[1] for item in subiter}
+    return res
+
+
+def Apriori(partition, support):
     '''
     :param partition: iterator
     :param support: int
     :return: tuple(tuple(int, tuple))
     '''
-    frequent = {}
-    cand_1 = {}
+
     temp_partition = list(partition)
-    print('temp_partition')
+    cand_1 = {}
+    frequent = {}
+
+    business = []
+
+    for line in temp_partition:
+        temp = convert(line)
+        business.extend(temp)
+
+    own_business_list = accumulate(business)
+
     for line in temp_partition:
         for item in line[1]:
             if item not in cand_1:
                 cand_1[(item)] = 1
             else:
                 cand_1[(item)] += 1
-    cand_1 = {key: value for key, value in cand_1.items() if value >= threshold}
-    print('cand_1')
-    frequent[1] = [frozenset([key]) for key, value in cand_1.items() if value >= support]
+
+    frequent[1] = [[key] for key, value in cand_1.items() if value >= support]
+
     k = 2
-    print(k)
     while 1:
-        temp_candidate = create_candidates(frequent[k - 1], k)
-        temp_frequent = frequent_items(temp_partition, temp_candidate, support)
+        print("Creating Candidates and Frequent", k)
+        temp_frequent = frequent_items(own_business_list, k, frequent[k - 1], support)
         if len(temp_frequent) == 0:
             break
-        else:
-            frequent[k] = temp_frequent
-            k += 1
-    res = [(key, value) for key, value in frequent.items()]
+        frequent[k] = temp_frequent
+        k += 1
+    res = [(key, {frozenset(pair) for pair in value}) for key, value in frequent.items()]
+
     return res
 
-
-def global_frequent(line, candidates):
-    '''
-    :param line: list
-    :param candidates: list[set]
-    :return: list[tuple]
-    '''
-    res = []
-    for candidate in candidates:
-        length_set = candidate[0]
-        items = list(candidate[1])
-        counter = {}
-        for item in items:
-            if item.issubset(line):
-                if tuple(item) not in counter:
-                    counter[tuple(item)] = 1
-                else:
-                    counter[tuple(item)] += 1
-        res.append((length_set, counter))
-    return res
+# def global_frequent(line, candidates):
+#     '''
+#     :param line: list
+#     :param candidates: list[tuple]
+#     :return: list[tuple]
+#     '''
+#     res = []
+#     for candidate in candidates:
+#         length_set = candidate[0]
+#         items = list(candidate[1])
+#         counter = {}
+#         for item in items:
+#             if set(item).issubset(line):
+#                 if item not in counter:
+#                     counter[item] = 1
+#                 else:
+#                     counter[item] += 1
+#         res.append((length_set, counter))
+#     return res
--- pyspark/Yelp/SON_2/Try.py
+++ pyspark/Yelp/SON_2/Try.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 import Apriori as A
+from functools import reduce
 
 ss = SparkSession \
     .builder \
@@ -11,10 +12,9 @@ sc = ss.sparkContext
 
 path = "/Users/chengyinliu/D/2019_Spring/INF553_ Foundations and Applications of Data Mining/ASSIGNMENTS/WEEK_2/SON/task2_data.csv"
 # path = "/Users/chengyinliu/D/2019_Spring/INF553_ Foundations and Applications of Data Mining/ASSIGNMENTS/WEEK_2/SON/Data/small2.csv"
-smallRDD = sc.textFile(path)
-header = smallRDD.first()
 
-# smallRDD.foreach(print)
+userRDD = sc.textFile(path)
+header = userRDD.first()
 
 support = 50
 threshold = 70
@@ -23,61 +23,43 @@ createCombiner = (lambda line: [line])
 mergeValue = (lambda exist, new: exist + [new])
 mergeCombiner = (lambda exist1, exist2: exist1 + exist2)
 
-def convert(line):
-    res = []
-    for business in line[1]:
-        res.append((business, line[0]))
-    return res
-
-businessRDD = smallRDD.filter(lambda line: line != header) \
+userRDD = userRDD.filter(lambda line: line != header) \
     .map(lambda line: (line.split(',')[0], line.split(',')[1])) \
     .combineByKey(createCombiner, mergeValue, mergeCombiner) \
-    .filter(lambda line: len(line[1]) >= threshold) \
-    .flatMap(lambda line: convert(line)) \
+    .filter(lambda line: len(line[1]) >= threshold)
+
+userRDD.foreach(print)
+
+businessRDD = userRDD.flatMap(lambda line: A.convert(line)) \
     .groupByKey() \
-    .mapValues(list)
+    .mapValues(set)
 
-# business = businessRDD.filter(lambda line: len(line[1]) > support).collect()
+business = businessRDD.collect()
+businessdict = {item[0]: item[1] for item in business}
 
-businessdict = {item[0]:item[1] for item in business}
+numOfPar = userRDD.getNumPartitions()
 
-
-
-
-# businessRDD = smallRDD.filter(lambda line: line != header) \
-#     .map(lambda line: (line.split(',')[1], line.split(',')[0])) \
-#     .combineByKey(createCombiner, mergeValue, mergeCombiner) \
-#     .filter(lambda line: len(line[1]) >= threshold)
-
-
-#
-numOfPar = smallRDD.getNumPartitions()
-#
-candidates = smallRDD.mapPartitions(lambda partition: A.Apriori(partition, support / numOfPar)) \
-    .reduceByKey(lambda a, b: a + b) \
-    .map(lambda line: (line[0], set(frozenset(item) for item in line[1]))) \
-    .map(lambda line: (line[0], [tuple(item) for item in line[1]])) \
+candidates = userRDD.mapPartitions(lambda partition: A.Apriori(partition, support / numOfPar)) \
+    .reduceByKey(lambda a, b: a | b) \
     .collect()
-#
-# # print([item for item in candidates if item[0] == 1])
-#
-# # print("candidates", candidates)
-#
-# # sc.broadcast(candidates)
-#
-# # A = smallRDD.flatMap(lambda line: line[1]).collect()
-#
-# # print(A)
-#
-# frequent = smallRDD.flatMap(lambda line: A.global_frequent(line[1], candidates)) \
-#     .reduceByKey(
-#     lambda exist1, exist2: {key: exist1.get(key, 0) + exist2.get(key, 0) for key in set(exist1) | set(exist2)}) \
-#     .collect()
-# #
-# # for i in sorted(frequent):
-# #     print(sorted(map(sorted, list(i[1].keys()))))
-# #     print()
-# #
-# A = {item[0]: [key for key, value in item[1].items() if value >= support] for item in frequent}
-#
-# print(A)
+
+candidates = sorted([(key, sorted([list(sets) for sets in value])) for key, value in candidates])
+
+res = []
+for cand in candidates:
+    temp_1 = []
+
+    if cand[0] == 1:
+        for value in cand[1]:
+            if len(businessdict[value[0]]) >= support:
+                temp_1.append(value)
+    else:
+        for value in cand[1]:
+            temp = reduce(lambda a, b: a & b, (businessdict[x] for x in value))
+            if len(temp) >= support:
+                temp_1.append(value)
+    if len(temp_1) == 0:
+        break
+    res.append((cand[0], temp_1))
+
+print(res)
