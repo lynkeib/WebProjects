33d445e555eff3823ff0b71d56ca08ca7b7fed55
lynkeib <liuchengyin1234@126.com>
2019-09-22 16:30:01 -0700

    .

--- pyspark/Recommendation System/main.py
+++ pyspark/Recommendation System/main.py
@@ -4,14 +4,14 @@ from utils import *
 
 
 def main():
-    b = 50
-    r = 3
     train_path = "./Data/yelp_train.csv"
     validation_path = "./Data/yelp_val.csv"
+    test_path = "./Data/test.csv"
 
     sc = SparkContext("local[*]", "RS")
 
     trainRDD = sc.textFile(train_path)
+    # trainRDD = sc.textFile(test_path)
     valRDD = sc.textFile(validation_path)
 
     header = trainRDD.first()
@@ -27,40 +27,67 @@ def main():
         .mapValues(dict)
 
     header = valRDD.first()
-    valRDD = valRDD.filter(lambda x: x != header) \
+    val_User_RDD = valRDD.filter(lambda x: x != header) \
         .map(lambda line: line.split(",")) \
         .map(lambda lst: (lst[0], [(lst[1], float(lst[2]))])) \
+        .reduceByKey(lambda a, b: a + b) \
+        .mapValues(dict)
+    val_Business_RDD = valRDD.filter(lambda x: x != header) \
+        .map(lambda line: line.split(",")) \
+        .map(lambda lst: (lst[1], [(lst[0], float(lst[2]))])) \
         .reduceByKey(lambda a, b: a + b) \
         .mapValues(dict)
     # trainRDD.foreach(print)
     users_business_rating = dict(user_business.collect())
     business_user_rating = dict(business_user.collect())
+    val_user = dict(val_User_RDD.collect())
+    val_business = dict(val_Business_RDD.collect())
+
+    # -------------------------------------- USER BASED START -------------------------------------- #
     user_average_rating = {user: float(sum(business_rating.values())) / len(business_rating.values()) for
                            user, business_rating in users_business_rating.items()}
-    for key, value in users_business_rating.items():
-        print('key', key, 'value', value)
-    for key, value in user_average_rating.items():
-        print('key', key, 'value', value)
-    print(Predict_User_Rating_On_a_Business("sdLns7062kz3Ur_b8wgeYw",
-                                            "9S7RUjzkdpU4MDxUxkvLvg",
-                                            users_business_rating,
-                                            business_user_rating,
-                                            user_average_rating))
+    TOP_N = 10
+    differences = []
+    for user in val_user:
+        for business in val_user[user]:
+            prediction = Predict_User_Rating_On_a_Business(user,
+                                                           business,
+                                                           TOP_N,
+                                                           users_business_rating,
+                                                           business_user_rating,
+                                                           user_average_rating)
+            differences.append((val_user[user][business] - prediction) ** 2)
+    print(sum(differences) / len(differences))
+    # -------------------------------------- USER BASED END -------------------------------------- #
 
-    # all_business = list(set(key for value in users.values() for key in value.keys()))
-    #
-    #
-    #
-    # business_order = {all_business[index]: index for index in range(len(all_business))}
-    # # print(business_order)
-    # # print(users)
-    # MinHashMatrix = buildMinHashMatrix(users, business_order, b * r)
-    # for key, value in MinHashMatrix.items():
-    #     print(key, value)
-    # user_set = set(users.keys())
-    # candidates = findCandidates(user_set, MinHashMatrix, b, r)
-    # print(candidates)
-
+    # -------------------------------------- ITEM BASED START -------------------------------------- #
+    ## Jaccard Similarity based LSH to find candidates
+    b = 50
+    r = 3
+    h = b * r
+    business_average_rating = {business: float(sum(user_rated.values())) / len(user_rated.values()) for
+                               business, user_rated
+                               in business_user_rating.items()}
+    user_order = {user: order for order, user in enumerate(users_business_rating.keys())}
+    MinHashMatrix = buildMinHashMatrix(business_user_rating, user_order, h)
+    candidates = findCandidates_LSH(business_user_rating, MinHashMatrix, b, r)
+    for index, value in candidates.items():
+        print('index', index, "value", value)
+    TOP_N = 5
+    differences = []
+    for business in val_business:
+        for user in val_business[business]:
+            prediction = Predict_Business_Rating_by_a_User(business,
+                                                           user,
+                                                           candidates,
+                                                           TOP_N,
+                                                           users_business_rating,
+                                                           business_user_rating,
+                                                           business_average_rating,
+                                                           user_average_rating)
+            differences.append((val_business[business][user] - prediction) ** 2)
+    print(sum(differences) / len(differences))
+    # -------------------------------------- ITEM BASED END -------------------------------------- #
 
 if __name__ == "__main__":
     main()
--- pyspark/Recommendation System/utils.py
+++ pyspark/Recommendation System/utils.py
@@ -1,20 +1,10 @@
 import random
 from itertools import combinations
+from collections import defaultdict
 
 
-def JaccardSimilarity(user1, user2):
-    """
-    :param user1: {business_id1: rating1, business_id2: rating2}
-    :param user2: {business_id1: rating1, business_id2: rating2}
-    :return: float
-    """
-    user1_set = set(user1.keys())
-    user2_set = set(user2.keys())
-    return len((user1_set & user2_set)) / float(len(user1_set | user2_set))
-
-
-## User-Baesd
-def Calculate_Pearson_Similarity(user1, user2, user_business_rating):
+# -------------------------------------- USER BASED HELPER FUNCTIONS START -------------------------------------- #
+def Calculate_Pearson_Similarity_User(user1, user2, user_business_rating):
     """
     :param user1: string
     :param user2: string
@@ -29,7 +19,7 @@ def Calculate_Pearson_Similarity(user1, user2, user_business_rating):
     if len(co_rated_businesses) == 0:
         return 0.0
 
-    # Step 2: calculate the average rating on ro_rated_business
+    # Step 2: calculate the average rating on co_rated_business
     user1_co_rated_average = float(sum(
         rating for business, rating in user_business_rating[user1].items() if business in co_rated_businesses)) / len(
         co_rated_businesses)
@@ -44,43 +34,56 @@ def Calculate_Pearson_Similarity(user1, user2, user_business_rating):
         (user_business_rating[user1][business] - user1_co_rated_average) ** 2 for business in co_rated_businesses) ** (
                           1.0 / 2) * sum(
         (user_business_rating[user2][business] - user2_co_rated_average) ** 2 for business in co_rated_businesses) ** (
-                              1.0 / 2)
+                          1.0 / 2)
     ## if Denominator is 0
-    if Denominator == 0:
-        return 0.0
+    if Denominator == 0 or Numerator == 0:
+        return 1.0
     else:
         return Numerator / Denominator
 
 
-def Predict_User_Rating_On_a_Business(user, business, user_business_rating, business_user_rating, user_average_rating):
+def Predict_User_Rating_On_a_Business(user, business, TOP_N, user_business_rating, business_user_rating,
+                                      user_average_rating):
     """
     :param user: string
     :param business: string
+    :param TOP_N: int
     :param user_business_rating: {user1: {business_id1: rating1, business_id2: rating2}, user2: {business_id1: rating1, business_id2: rating2}...}
     :param business_user_rating: {business_id1: {user1: rating1, user2: rating2}, business_id2: {user1: rating1, user2: rating2}...}
     :param user_average_rating: {user1: rating1, user2: rating2...}
     :return: float
     """
     # Step 1: find users who rated this business
+    ## if this business never showed up, then use the user average rating
+    if business not in business_user_rating:
+        return user_average_rating[user]
     users_rated_this_business = set(business_user_rating[business].keys())
-    ## if not users rated this business, then use the user average as an estimation
+    ## if no users rated this business, then use the user average as an estimation
     if len(users_rated_this_business) == 0:
         return user_average_rating[user]
 
     # Step 2: calculate the Pearson Correlation Coefficient between them
-    pearson_correlation = dict()
+    # pearson_correlation = dict()
+    sorted_users = []
     for other_user in users_rated_this_business:
-        pearson_correlation[other_user] = Calculate_Pearson_Similarity(user, other_user, user_business_rating)
-    print("pearson_correlation is")
-    for key, value in pearson_correlation.items():
-        print('key', key, 'value', value)
+        pearson_correlation_value = Calculate_Pearson_Similarity_User(user, other_user, user_business_rating)
+        # pearson_correlation[other_user] = Calculate_Pearson_Similarity(user, other_user, user_business_rating)
+        sorted_users.append((pearson_correlation_value, other_user))
+    # print("pearson_correlation is")
+    # for key, value in pearson_correlation.items():
+    #     print('key', key, 'value', value)
 
+    # Step 3: Select Top N Users (Neighborhood)
+    sorted_users.sort(key=lambda pair: abs(pair[0]), reverse=True)
+    Top_N_users = sorted_users[:TOP_N]
+    # print(Top_N_users)
     # Step 3: Calculate Numerator and Denominator
     Numerator = sum(
-        (user_business_rating[other_user][business] - user_average_rating[other_user]) * pearson_correlation[other_user]
-        for other_user in users_rated_this_business)
-    print('Numerator is', Numerator)
-    Denominator = sum(abs(pearson_correlation[other_user]) for other_user in users_rated_this_business)
+        (user_business_rating[other_user][business] - user_average_rating[other_user]) * pearson_correlation_value
+        for pearson_correlation_value, other_user in Top_N_users)
+    # print('Numerator is', Numerator)
+    Denominator = sum(abs(pearson_correlation_value) for pearson_correlation_value, other_user in Top_N_users)
+    # print("Denominator is", Denominator)
     ## if Denominator is 0, then the best estimation is the average of this user
     if Denominator == 0:
         return user_average_rating[user]
@@ -88,53 +91,191 @@ def Predict_User_Rating_On_a_Business(user, business, user_business_rating, busi
         return user_average_rating[user] + (float(Numerator) / Denominator)
 
 
+# -------------------------------------- USER BASED HELPER FUNCTIONS END -------------------------------------- #
+
+# -------------------------------------- ITEM BASED HELPER FUNCTIONS START -------------------------------------- #
+
 def hash_generator(num):
     hash_functions = [(random.choice(range(1, 1001)), random.choice(range(1, 1001))) for _ in range(num)]
     return hash_functions
 
 
-def buildMinHashMatrix(users, business_order, numer_of_hashfunc):
+def buildMinHashMatrix(business_user_rating, user_order, numer_of_hashfunc):
     """
-    :param users: {user1: {business_id1: rating1, business_id2: rating2}, user2: {business_id1: rating1, business_id2: rating2}...}
-    :param business_order: {business_id1: order1, business_id2: order2...}
+    :param business_user_rating: {business_id1: {user1: rating1, user2: rating2}, business_id2: {user1: rating1, user2: rating2}...}
+    :param user_order: {user1: order1, user2: order2...}
     :param numer_of_hashfunc: int
     :return: {user1: [order3, order2...], user2: [order4, order15...]}
     """
     hash_functions = hash_generator(numer_of_hashfunc)
     MinHashMatrix = dict()
-    business_number = len(business_order.keys())
-    for user in users.keys():
-        MinHashMatrix[user] = list()
+    number_of_total_users = len(user_order)
+    count = 1
+    total_business = len(business_user_rating)
+    for business in business_user_rating.keys():
+        print(f"building {business}'s signature, left {total_business - count}")
+        MinHashMatrix[business] = list()
         for hash in hash_functions:
-            MinHashMatrix[user].append(
-                min((hash[0] * business_order[business_id] + hash[1]) % business_number for business_id in
-                    users[user].keys()))
+            MinHashMatrix[business].append(
+                min((hash[0] * user_order[user] + hash[1]) % number_of_total_users for user in
+                    business_user_rating[business].keys()))
+        count += 1
     return MinHashMatrix
 
 
-def findCandidates(users_set, MinHashMatrix, b, r):
+def JaccardSimilarity(business1, business2, business_user_rating):
     """
-    :param users: {user1, user2, user3...}
-    :param MinHashMatrix: {user1: [order3, order2...], user2: [order4, order15...]}
+    :param business1: string
+    :param business2: string
+    :param business_user_rating: {business_id1: {user1: rating1, user2: rating2}, business_id2: {user1: rating1, user2: rating2}...}
+    :return: float
+    """
+    user_set_buisness1 = set(business_user_rating[business1].keys())
+    user_set_buisness2 = set(business_user_rating[business2].keys())
+    return len((user_set_buisness1 & user_set_buisness2)) / float(len(user_set_buisness1 | user_set_buisness2))
+
+
+def findCandidates_LSH(business_user_rating, MinHashMatrix, b, r):
+    """
+    :param business_user_rating: {business_id1: {user1: rating1, user2: rating2}, business_id2: {user1: rating1, user2: rating2}...}
+    :param MinHashMatrix: {business1: [signature1, signature2...], business2: [signature1, signature2...]}
     :param b: int
     :param r: int
-    :return: list
+    :return: {business1: {business2, business4}, business2: {business13}...}
     """
-    start = 0
-    candidates = []
-    for end in range(r, r * b, r):
-        print(start, end)
-        print(len(users_set))
-        if not users_set:
-            break
-        remove_users = set()
-        for comb in combinations(users_set, 2):
-            user1, user2 = comb
-            if MinHashMatrix[user1][start:end] == MinHashMatrix[user2][start:end]:
-                candidates.append((user1, user2))
-                remove_users.add(user1)
-                remove_users.add(user2)
-                print('cand', (user1, user2))
-        users_set = users_set - remove_users
-        start = end
+    ## since we only consider two business ids who have at least one identical signature in on band
+    ## therefore, we first create all the bands, then, create hash busket for each band
+
+    # Step 1: Create bands
+    print('Creating bands')
+    bands = dict()
+    for band_index in range(b):
+        bands[band_index] = defaultdict(list)
+
+    # Step 2: In each band, collect the business ids who's signatures are identical
+    print('hashing signatures in each bands')
+    for band_index in range(b):
+        print(f'----hashing band {band_index}, left {b - band_index - 1}')
+        for business in business_user_rating.keys():
+            business_signature_in_this_band = tuple(MinHashMatrix[business][band_index:band_index + r])
+            bands[band_index][business_signature_in_this_band].append(business)
+
+    # Step 3: Collect candidates in each band who hashed into the same basket
+    print('Creating candidates')
+    candidates_LSH = set()
+    for band_index in range(b):
+        print(f'----creating candidates in band {band_index}, left {b - band_index - 1}')
+        for band_signature in bands[band_index]:
+            bands_candidates = bands[band_index][band_signature]
+            if len(bands_candidates) > 1:
+                for comb in combinations(bands_candidates, 2):
+                    candidates_LSH.add(frozenset(comb))
+
+    # Step 4: Use Jaccard Similarity to do the further filter
+    candidates_Jaccard = []
+    for candidate in candidates_LSH:
+        candidate = tuple(candidate)
+        if JaccardSimilarity(candidate[0], candidate[1], business_user_rating) >= 0.5:
+            candidates_Jaccard.append(candidate)
+
+    # Step 5: Integrate the result into a dictionary
+    candidates = defaultdict(set)
+    for candidate in candidates_Jaccard:
+        business1 = candidate[0]
+        business2 = candidate[1]
+        candidates[business1].add(business2)
+        candidates[business2].add(business1)
+
     return candidates
+
+
+def Calculate_Pearson_Similarity_Business(business1, business2, business_user_rating, candidates):
+    """
+    :param business1: string
+    :param business2: string
+    :param business_user_rating: {business_id1: {user1: rating1, user2: rating2}, business_id2: {user1: rating1, user2: rating2}...}
+    :param candidates: {business1: {business2, business4}, business2: {business13}...}
+    :return: float
+    """
+    # Step 0: determine whether this two business is in our candidates
+    if business2 not in candidates[business1]:
+        return 0.0
+
+    # Step 1: find co-rated users
+    business1_rated_by_users = set(business_user_rating[business1].keys())
+    business2_rated_by_users = set(business_user_rating[business2].keys())
+    co_rated_users = business1_rated_by_users & business2_rated_by_users
+    ## if do not have co_rated_businesses, then the Similarity is 0 (should not happen)
+    if len(co_rated_users) == 0:
+        return 0.0
+
+    # Step 2: calculate the average rating rated by co_rated_users
+    business1_rated_by_co_users = float(sum(
+        rating for user, rating in business_user_rating[business1].items() if user in co_rated_users)) / len(
+        co_rated_users)
+    business2_rated_by_co_users = float(sum(
+        rating for user, rating in business_user_rating[business2].items() if user in co_rated_users)) / len(
+        co_rated_users)
+
+    # Step 3: calculate Numerator and Denominator
+    Numerator = float(sum((business_user_rating[business1][user] - business1_rated_by_co_users) * (
+            business_user_rating[business2][user] - business2_rated_by_co_users) for user in co_rated_users))
+    Denominator = sum(
+        (business_user_rating[business1][user] - business1_rated_by_co_users) ** 2 for user in co_rated_users) ** (
+                          1.0 / 2) * sum(
+        (business_user_rating[business2][user] - business2_rated_by_co_users) ** 2 for user in co_rated_users) ** (
+                          1.0 / 2)
+    ## if Denominator is 0
+    if Denominator == 0 or Numerator == 0:
+        return 1.0
+    else:
+        return Numerator / Denominator
+
+
+def Predict_Business_Rating_by_a_User(business, user, candidates, TOP_N, user_business_rating, business_user_rating,
+                                      business_average_rating, user_average_rating):
+    """
+    :param business: string
+    :param user: string
+    :param candidates: {business1: {business2, business4}, business2: {business13}...}
+    :param TOP_N: int
+    :param user_business_rating: {user1: {business_id1: rating1, business_id2: rating2}, user2: {business_id1: rating1, business_id2: rating2}...}
+    :param business_user_rating: {business_id1: {user1: rating1, user2: rating2}, business_id2: {user1: rating1, user2: rating2}...}
+    :param business_average_rating: {business1: rating1, business2: rating2...}
+    :param user_average_rating: {user1: rating1, user2: rating2...}
+    :return: float
+    """
+    # Step 1: find businesses this user rated
+    ## if this business never showed up, then use the user average rating
+    if business not in business_user_rating:
+        return user_average_rating[user]
+    ## if this user never showed up, then use the business average rating
+    if user not in user_business_rating:
+        return business_average_rating[business]
+    businesses_rated_by_this_user = set(user_business_rating[user].keys())
+
+    # Step 2: calculate the Pearson Correlation Coefficient between them (only consider the business in candidates)
+    sorted_businesses = []
+    for other_business in businesses_rated_by_this_user:
+        pearson_correlation_value = Calculate_Pearson_Similarity_Business(business, other_business,
+                                                                          business_user_rating, candidates)
+        sorted_businesses.append((pearson_correlation_value, other_business))
+    # print("pearson_correlation is")
+    # for key, value in pearson_correlation.items():
+    #     print('key', key, 'value', value)
+
+    # Step 3: Select Top N Users (Neighborhood)
+    sorted_businesses.sort(key=lambda pair: abs(pair[0]), reverse=True)
+    Top_N_businesses = sorted_businesses[:TOP_N]
+    # print(Top_N_users)
+    # Step 3: Calculate Numerator and Denominator
+    Numerator = sum(business_user_rating[other_business][user] * pearson_correlation_value for pearson_correlation_value, other_business in Top_N_businesses)
+    # print('Numerator is', Numerator)
+    Denominator = sum(abs(pearson_correlation_value) for pearson_correlation_value, other_business in Top_N_businesses)
+    # print("Denominator is", Denominator)
+
+    ## if Denominator is 0, then the best estimation is the average of this user
+    if Denominator == 0:
+        return business_average_rating[business]
+    else:
+        return float(Numerator) / Denominator
