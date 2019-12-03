424f6c5fbae4d853f4e60b3277e74f47b9943c3b
lynkeib <liuchengyin1234@126.com>
2019-12-03 11:37:02 -0800

    Update

--- Forecasting System/Temperature_Prediction/Models/HW.py
+++ Forecasting System/Temperature_Prediction/Models/HW.py
@@ -3,20 +3,54 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 import pandas as pd
 import numpy as np
 
+from Helper import helper
+
 
 class HW(object):
 
     def __init__(self, dataframe):
         df = dataframe.copy()
+        self.data = helper.HW_Temp_data_cleaning(df)
 
     def set_date(self, date):
         self.date = date
 
     def model_building(self, training_data, station):
-        pass
+        model = ExponentialSmoothing(training_data, seasonal='add', seasonal_periods=24 * 365)
+        model_fit = model.fit()
+        return model_fit
 
     def model_selection_mape_rmse(self, station):
-        pass
+        datetime = pd.to_datetime(self.date) - timedelta(days=2)
+        train_end = datetime + timedelta(hours=7)
+        test_start = datetime + timedelta(hours=8)
+        test_end = datetime + timedelta(days=1) + timedelta(hours=23)
+
+        train = self.data[station + '_Temp'].loc[:train_end]
+        test = self.data[station + '_Temp'].loc[test_start:test_end]
+        model = self.model_building(train, station)
+        yhat = model.forecast(40)
+        yhat.index = test.index
+        # self.mape = abs((yhat - test) / test).mean()
+        # self.rmse = np.sqrt(((yhat - test) ** 2).mean())
+        self.mape = helper.mape(test, yhat)
+        self.rmse = helper.rmse(test, yhat)
 
     def predict_next_40hours_temp(self, station):
-        pass
+        train_end2 = pd.to_datetime(self.date) + timedelta(hours=7)
+        train2 = self.data[station + '_Temp'].loc[:train_end2]
+        model2 = self.model_building(train2, station)
+        y_predict = model2.forecast(40)
+        self.forecast = y_predict.to_list()
+
+
+if __name__ == '__main__':
+    path = '../../Data/Hourly_Temp_Humi_Load-6.csv'
+    df = pd.read_csv(path)
+    model_DR = HW(df)
+    model_DR.set_date('2018-09-01')
+    station = 'TRM'
+    model_DR.model_selection_mape_rmse(station)
+    print(f'MAPE: {model_DR.mape}, RMSE: {model_DR.rmse}')
+    model_DR.predict_next_40hours_temp(station)
+    print(model_DR.forecast)
--- Forecasting System/Temperature_Prediction/TempPred.py
+++ Forecasting System/Temperature_Prediction/TempPred.py
@@ -1,16 +1,27 @@
 from Temperature_Prediction.Models.DR import DR
+from Temperature_Prediction.Models.HW import HW
 
 
 class TempPred(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, dataframe):
+        self.DR = DR(dataframe)
+        self.HW = HW(dataframe)
+        self.models = [self.DR, self.HW]
 
     def model_building(self, date, station):
-        pass
+        self.MAPE = []
+        self.RMSE = []
+        for model in self.models:
+            model.set_date(date)
+            model.model_selection_mape_rmse(station)
+            self.MAPE.append(model.mape)
+            self.RMSE.append(model.rmse)
 
     def ensemble_models(self):
-        pass
+        index = self.MAPE.index(min(self.MAPE))
+        self.model = self.models[index]
 
-    def predict_next_40hours_temp(self):
-        pass
+    def predict_next_40hours_temp(self, station):
+        prediction = self.model.predict_next_40hours_temp(station)
+        return prediction
