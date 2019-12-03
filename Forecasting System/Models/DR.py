6e64bf21aed0aa4790a704c6ef79b607abdc980a
lynkeib <liuchengyin1234@126.com>
2019-12-03 10:03:18 -0800

    Re-structure Forecasting Sys

---
+++ Forecasting System/Models/DR.py
@@ -0,0 +1,10 @@
+class DR(object):
+
+    def __init__(self):
+        pass
+
+    def model_building(self):
+        pass
+
+    def predict_next_40_hours(self):
+        pass
---
+++ Forecasting System/Models/TM.py
@@ -0,0 +1,10 @@
+class TM(object):
+
+    def __init__(self):
+        pass
+
+    def model_building(self):
+        pass
+
+    def predict_next_40_hours(self):
+        pass
