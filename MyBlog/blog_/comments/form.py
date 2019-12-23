976df7196012e31d0abe0648de8e73b1a1ebf3ba
lynkeib <liuchengyin1234@126.com>
2019-12-23 09:18:33 -0800

    Update

--- MyBlog/blog_/comments/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Comments
-
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comments
-        fields = ['name', 'email', 'url', 'text']
