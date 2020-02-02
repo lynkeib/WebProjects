2785f5185e1266d03cd226c38cca3c42183a7684
lynkeib <liuchengyin1234@126.com>
2020-02-02 15:38:03 -0800

    Color Guessing

--- WebDevelopment/Projects/Color Guessing/color.js
+++ WebDevelopment/Projects/Color Guessing/color.js
@@ -1,4 +1,4 @@
-var color = [
+var colors = [
     "rgb(255, 255, 0)",
     "rgb(255, 0, 255)",
     "rgb(0, 255, 255)",
@@ -9,9 +9,23 @@ var color = [
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
-
+var colordisplay = document.getElementById("colordisplay");
+colordisplay.textContent = pickedColor;
 
 
 for(var i = 0; i < squares.length; i++){
-    squares[i].style.backgroundColor = color[i];
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].addEventListener("click", function(){
+        if(this.style.backgroundColor === pickedColor){
+            success();
+        }else{
+            this.style.backgroundColor = "#232323";
+        };
+    });
 }
+
+function success(){
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = pickedColor;
+    };
+};
