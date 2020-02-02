da7d917e702cdedcf4afcd29eba78cbe8aa50e46
lynkeib <liuchengyin1234@126.com>
2020-02-02 12:43:27 -0800

    Score Kepper

---
+++ WebDevelopment/Class_Practice/Score Kepper/scorekepper.js
@@ -0,0 +1,58 @@
+var input = document.querySelectorAll("input")[0];
+var playerone_button = document.querySelectorAll("#playerone-button")[0];
+var playertwo_button = document.querySelectorAll("#playertwo-button")[0];
+var playerone_score = 0;
+var playertwo_score = 0;
+var playingto = document.querySelectorAll("#playingto")[0];
+var reset = document.querySelectorAll("#reset")[0];
+
+input.addEventListener("input", function(){
+    playingto.textContent = Number(input.value);
+});
+
+playerone_button.addEventListener("click", function(){
+    if(playerone_score + playertwo_score == playingto.textContent){
+        return;
+    }
+    playerone_score++;
+    document.querySelectorAll("#playerone")[0].textContent = playerone_score;
+    console.log(playerone_score + playertwo_score, playingto);
+    if(playerone_score + playertwo_score == playingto.textContent){
+        if(playerone_score > playertwo_score){
+            document.querySelectorAll("#playerone")[0].classList.add("winer");
+        }
+        else if(playerone_score < playertwo_score) {
+            document.querySelectorAll("#playertwo")[0].classList.add("winer")
+        };
+    };
+});
+
+playertwo_button.addEventListener("click", function(){
+    if(playerone_score + playertwo_score == playingto.textContent){
+        return;
+    }
+    playertwo_score++;
+    document.querySelectorAll("#playertwo")[0].textContent = playertwo_score;
+    console.log(playerone_score + playertwo_score, playingto);
+    if(playerone_score + playertwo_score == playingto.textContent){
+        if(playerone_score > playertwo_score){
+            document.querySelectorAll("#playerone")[0].classList.add("winer");
+        }
+        else if(playerone_score < playertwo_score) {
+            document.querySelectorAll("#playertwo")[0].classList.add("winer")
+        };
+    };
+});
+
+reset.addEventListener("click", function(){
+    if(playerone_score > playertwo_score){
+        document.querySelectorAll("#playerone")[0].classList.remove("winer");
+    }
+    else if(playerone_score < playertwo_score){
+        document.querySelectorAll("#playertwo")[0].classList.remove("winer");
+    };
+    playerone_score = 0;
+    playertwo_score = 0;
+    document.querySelectorAll("#playerone")[0].textContent = 0;
+    document.querySelectorAll("#playertwo")[0].textContent = 0;
+});
