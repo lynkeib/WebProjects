e420915b090149c9d8b4ba2c08c778998ea00806
lynkeib <liuchengyin1234@126.com>
2020-02-02 15:56:11 -0800

    Update

--- WebDevelopment/Projects/Color Guessing/color.js
+++ WebDevelopment/Projects/Color Guessing/color.js
@@ -1,25 +1,38 @@
-var colors = [
-    "rgb(255, 255, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(0, 255, 255)",
-    "rgb(143, 255, 0)",
-    "rgb(0, 43, 23)",
-    "rgb(255, 2, 0)"
-];
+var colorNum = 6;
+var colors = colorGen(colorNum);
+
+function colorGen(colorNum){
+    var colors = [];
+    for(var i = 0; i < colorNum; i++){
+        colors.push(colorRandom())
+    }
+    return colors;
+}
+
+function colorRandom(){
+    var R = String(Math.floor(Math.random() * 256));
+    var G = String(Math.floor(Math.random() * 256));
+    var B = String(Math.floor(Math.random() * 256));
+    return "rgb(" + R + ", " + G + ", " + B + ")";
+}
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = colors[Math.floor(Math.random() * colors.length)];
 var colordisplay = document.getElementById("colordisplay");
 colordisplay.textContent = pickedColor;
+var message = document.getElementById("message");
+
 
 
 for(var i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
     squares[i].addEventListener("click", function(){
         if(this.style.backgroundColor === pickedColor){
-            success();
+            success()
+            message.textContent = "Correct";;
         }else{
             this.style.backgroundColor = "#232323";
+            message.textContent = "Try again";
         };
     });
 }
