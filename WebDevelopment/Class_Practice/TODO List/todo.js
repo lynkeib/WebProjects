275c88f2683225ccdccf7f2c4f2b04a511d6a0dc
lynkeib <liuchengyin1234@126.com>
2020-02-02 01:35:07 -0800

    Update todo.js

--- WebDevelopment/Class_Practice/TODO List/todo.js
+++ WebDevelopment/Class_Practice/TODO List/todo.js
@@ -3,6 +3,7 @@ Todo List
 - "new" - Add a todo
 - "list" - View all todos
 - "quit" - Quit App
+- "delete" - Remove a todo
 */
 var command = null;
 var todo = [];
@@ -13,6 +14,16 @@ while(command !== "quit"){
     }
     else if(command === "list") {
         console.log(todo);
+    }
+    else if (command === "delete"){
+        var remove_index = prompt("Enter index of todo to delete")
+        if(remove_index < 0 || remove_index >= todo.length){
+            console.log("No such index");
+            continue;
+        }
+        else{
+            todo.splice(remove_index);
+        }
     }
     else {
         break;
