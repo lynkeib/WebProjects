bc7eb2a562d39bc23fc88afd6df4cb52d04058e0
lynkeib <liuchengyin1234@126.com>
2020-02-03 20:46:00 -0800

    Update jQuery.js

--- WebDevelopment/Class_Practice/jQuery/jQuery.js
+++ WebDevelopment/Class_Practice/jQuery/jQuery.js
@@ -1,4 +1,14 @@
 // $ for selection
+// $("h1:first-of-type")， select the first element in this type, nth-of-type
 // .text(), get all the text in the label, or change the text
 // .html(), get the html contents in the selection
 // .css(), change css for the selection
+// .attr(), change or add attributes, can do multiple adding
+// .val(), wrapper for .value using for input tag and select tag, works on text, color, checkbox
+// .addClass(), .removeClass(), .toggleCLass()
+
+// Events
+// .click(), add click listern
+// ,Keypress(), will be tragged if the user press a key, or press a certain key (input for the function should be "event")
+// .on(), can replace all the events methods, .on("click", function(){})
+// this in jQuery is $(this)
