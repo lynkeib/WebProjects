ebb348504febe8042469027c337b6df6c694d76e
lynkeib <liuchengyin1234@126.com>
2020-02-05 23:05:22 -0800

    TODO Done

--- WebDevelopment/Projects/TODO list/todo.js
+++ WebDevelopment/Projects/TODO list/todo.js
@@ -1,7 +1,22 @@
 $("#textinput").keypress(function(event){
     if(event.keyCode == 13){
         // console.log(event)
-        $("ul").append("<li>" + $("#textinput").val() + "</li>");
+        $("ul").append("<li><span><i class='far fa-trash-alt'></i></span>" + $("#textinput").val() + "</li>");
         $("#textinput").val("");
     }   
+})
+
+$(".fa-plus").on("click", function(){
+    $("input").slideToggle();
+})
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("corssthetext");
+})
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(200, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
 })
