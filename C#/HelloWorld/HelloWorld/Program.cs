3074593600646d3b603deae10dcde8e90741ae8c
lynkeib <liuchengyin1234@126.com>
2020-02-09 11:25:30 -0800

    A

---
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            al.Add(1);
+            al.Add("ss");
+            foreach (var i in al)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
