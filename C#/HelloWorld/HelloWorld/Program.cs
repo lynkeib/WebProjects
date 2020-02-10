c1b24ff9677fe91387e2a058b47e5cba563a1b43
lynkeib <liuchengyin1234@126.com>
2020-02-09 20:59:40 -0800

    Reference, Out

--- C#/HelloWorld/HelloWorld/Program.cs
+++ C#/HelloWorld/HelloWorld/Program.cs
@@ -7,15 +7,53 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var student = new Student();
-            student.Name = Util.Console.Ask("Name?");
-            System.Console.Write(student.Name);
+            var name = Util.Console.Ask("Name?");
+            var grade = int.Parse(Util.Console.Ask("Grade?"));
+            var birthday = Util.Console.Ask("Birthday?");
+            var student = new Student(name, grade, birthday);
+            //student.Name = Util.Console.Ask("Name?");
+            student.print();
         }
     }
 
-    class Student
+    class Member
     {
-        public string Name { set; get; }
-        public int Phone { set; get; }
+        public string Name;
+        public int Grade;
+        protected string Birthday { set; get; }
+
+        public void print()
+        {
+            System.Console.WriteLine(Name);
+            System.Console.WriteLine(Grade);
+            System.Console.WriteLine(Birthday);
+        }
+    }
+
+    class Student : Member
+    {
+        public Student(string name, int grade, string birthday)
+        {
+            System.Console.WriteLine("Creating Student");
+            Name = name;
+            Grade = grade;
+            Birthday = birthday;
+        }
+        ~Student()
+        {
+            System.Console.Write("Deleting Student");
+        }
+        
+    }
+
+    class Teacher : Member
+    {
+        public Teacher(string name, int grade, string birthday)
+        {
+            System.Console.WriteLine("Creating Teacher");
+            Name = name;
+            Grade = grade;
+            Birthday = birthday;
+        }
     }
 }
