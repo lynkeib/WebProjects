605854b5eb82c991a7f7f489d01c0116b3e2799f
lynkeib <liuchengyin1234@126.com>
2020-02-22 11:58:51 -0800

    Views

---
+++ C#/Projects/LanzhouBeefNoodles/LanzhouBeefNoodles/controllers/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace LanzhouBeefNoodles.controllers
+{
+    public class Test: Controller
+    {
+        public ActionResult index()
+        {
+            return Content("Hello from test index");
+        }
+
+        public String About()
+        {
+            return "Hello from test about";
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
