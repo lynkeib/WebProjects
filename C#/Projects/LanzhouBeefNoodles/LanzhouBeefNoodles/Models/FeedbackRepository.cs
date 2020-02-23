161871eeb0de87677bfad5805fc6697e0b639270
lynkeib <liuchengyin1234@126.com>
2020-02-22 22:52:14 -0800

    Database

---
+++ C#/Projects/LanzhouBeefNoodles/LanzhouBeefNoodles/Models/FeedbackRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LanzhouBeefNoodles.Models
+{
+    public class FeedbackRepository: IFeedbackRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public FeedbackRepository(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void AddFeedback(Feedback feedback)
+        {
+            _context.Feedbacks.Add(feedback);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Feedback> GetAllFeedbacks()
+        {
+            return _context.Feedbacks;
+        }
+    }
+}
--- C#/Projects/LanzhouBeefNoodles/LanzhouBeefNoodles/Startup.cs
+++ C#/Projects/LanzhouBeefNoodles/LanzhouBeefNoodles/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using LanzhouBeefNoodles.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 
 namespace LanzhouBeefNoodles
@@ -17,11 +19,20 @@ namespace LanzhouBeefNoodles
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc(options => { options.EnableEndpointRouting = false; });
-            services.AddTransient<INoodleRepository, MockNoodleRepository>();
-            services.AddTransient<IFeedbackRepository, MockFeedbackRepository>();
+            services.AddTransient<INoodleRepository, NoodleRepository>();
+            services.AddTransient<IFeedbackRepository, FeedbackRepository>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
