86e98e74c5403d9b7d6234199a4b8063890e96e4
lynkeib <liuchengyin1234@126.com>
2020-05-19 11:31:50 -0700

    Update

--- React/projects/react-complete-guide/src/App.js
+++ React/projects/react-complete-guide/src/App.js
@@ -6,35 +6,37 @@ import Person from './Person/Person';
 class App extends Component {
 
   state = {
-    persons : [
-      {name:"Max", age:12},
-      {name:"1234", age:123},
-      {name:"12345", age:345}
+    persons: [
+      { name: "Max", age: 12 },
+      { name: "1234", age: 123 },
+      { name: "12345", age: 345 }
     ]
   }
 
   switchNameHandler = () => {
     // this.state.persons[0].name = "KKK";
-    this.setState({persons : [
-      {name:"KKKK", age:12},
-      {name:"1234", age:123},
-      {name:"12345", age:345}
-    ]});
+    this.setState({
+      persons: [
+        { name: "KKKK", age: 12 },
+        { name: "1234", age: 123 },
+        { name: "12345", age: 345 }
+      ]
+    });
   }
 
   render() {
-    const style={
-      backgroundColor:'white',
-      font:'inherit',
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor:'pointer'
+      cursor: 'pointer'
     };
     return (
       <div className="App">
         <h1>Test</h1>
         <button onClick={this.switchNameHandler} style={style}>Switch name</button>
-        <Person name={this.state.persons[0].name}/>
+        <Person name={this.state.persons[0].name} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Just test</Person>
       </div>
     );
