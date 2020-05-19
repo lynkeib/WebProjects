e432650b8ff3d81eda4cf936b54148f0f877d2c2
lynkeib <liuchengyin1234@126.com>
2020-05-19 14:17:28 -0700

    Update

--- React/README.md
+++ React/README.md
@@ -2,3 +2,4 @@
 	+ Everything in React is component
 	+ StateFul & StateLess (State can be used both in class and function)
 	+ When passing functions, it is recommended to pass the reference of the function (using .bind to pass args) rather than anonymous function for the sake of high performance 
+	+ Elements in .js that are similiar to HTML, they are not HTML, they are JXS, JS in HTML format. Therefore, some props names are different from HTML
--- React/projects/react-complete-guide/src/App.js
+++ React/projects/react-complete-guide/src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       { name: "Max", age: 12 },
       { name: "1234", age: 123 },
       { name: "12345", age: 345 }
-    ]
+    ],
+    hidden: false
   }
 
   switchNameHandler = () => {
@@ -24,6 +25,11 @@ class App extends Component {
     });
   }
 
+  togglePersonHandler = () => {
+    let doesShow = this.state.hidden;
+    this.setState({ hidden: !doesShow });
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -32,12 +38,22 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
+
+    let persons = null;
+    if (this.state.hidden) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Just test</Person>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Test</h1>
-        <button onClick={this.switchNameHandler} style={style}>Switch name</button>
-        <Person name={this.state.persons[0].name} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Just test</Person>
+        <button onClick={this.togglePersonHandler} style={style}>Switch name</button>
+        {persons}
       </div>
     );
   }
