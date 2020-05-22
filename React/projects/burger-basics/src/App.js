f1f812f9b93836120416b2c47d1e56d185384d83
lynkeib <liuchengyin1234@126.com>
2020-05-22 12:00:06 -0700

    Update

--- React/projects/burger-basics/src/App.js
+++ React/projects/burger-basics/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuildre/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <BurgerBuilder/>
+        </Layout>
       </div>
     );
   }
