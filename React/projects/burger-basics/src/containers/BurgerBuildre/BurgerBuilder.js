d03ab2e6c05dc6ceb6953a608e06813a597dd206
lynkeib <liuchengyin1234@126.com>
2020-05-29 10:30:27 -0700

    Add Router

--- React/projects/burger-basics/src/containers/BurgerBuildre/BurgerBuilder.js
+++ React/projects/burger-basics/src/containers/BurgerBuildre/BurgerBuilder.js
@@ -82,24 +82,25 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert("Continue");
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: "test",
-                address: {
-                    street: 'test',
-                    zipCode: '12345',
-                    contry: 'test'
-                },
-                email: '123@124.com'
-            },
-            deliveryMethod: 'slowest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => { this.setState({ loading: false, purchasing: false }) })
-            .catch(error => { this.setState({ loading: false, purchasing: false }) });
+        // this.setState({ loading: true });
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: "test",
+        //         address: {
+        //             street: 'test',
+        //             zipCode: '12345',
+        //             contry: 'test'
+        //         },
+        //         email: '123@124.com'
+        //     },
+        //     deliveryMethod: 'slowest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => { this.setState({ loading: false, purchasing: false }) })
+        //     .catch(error => { this.setState({ loading: false, purchasing: false }) });
+        this.props.history.push('/checkout');
     }
 
     render() {
