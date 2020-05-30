0de93a92f865e1f0f0c2d820e19424cf50bc8090
lynkeib <liuchengyin1234@126.com>
2020-05-30 11:13:24 -0700

    Form and Form Validation

--- React/projects/burger-basics/src/hoc/withErrorHandler/withErrorHandler.js
+++ React/projects/burger-basics/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Model from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
-import axios from '../../axios-orders'
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
