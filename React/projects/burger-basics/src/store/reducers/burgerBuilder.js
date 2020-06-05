2d5a93151265af1899d940f85015a6d9ce0fc17e
lynkeib <liuchengyin1234@126.com>
2020-06-05 11:04:40 -0700

    Redux

--- React/projects/burger-basics/src/store/reducers/burgerBuilder.js
+++ React/projects/burger-basics/src/store/reducers/burgerBuilder.js
@@ -17,23 +17,24 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
-            const updatedIngredient = {
-                [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-            }
-            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-            const updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            }
-            // return {
-            //     ...state,
-            //     ingredients: {
-            //         ...state.ingredients,
-
-            //     },
+            // const updatedIngredient = {
+            //     [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+            // }
+            // const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+            // const updatedState = {
+            //     ingredients: updatedIngredients,
             //     totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
             // }
-            return updateObject(state, updatedState)
+            // return updateObject(state, updatedState)
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                },
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+            }
+
         case actionTypes.REMOVE_INGREDIENT:
             return {
                 ...state,
