7c0c60b43ff7e1a6aac8eb0c6144dd10dd5ddc15
lynkeib <liuchengyin1234@126.com>
2020-06-05 14:22:02 -0700

    Authentication

--- React/projects/burger-basics/src/components/Navigation/NavigationItems/NavigationItems.js
+++ React/projects/burger-basics/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,7 @@ const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Checkout</NavigationItem>
+        <NavigationItem link="/auth">Auth</NavigationItem>
     </ul>
 )
 
