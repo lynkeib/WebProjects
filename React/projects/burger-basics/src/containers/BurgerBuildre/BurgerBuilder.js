5272c1011cb850b68de2ab42dedd660fed96ff4f
lynkeib <liuchengyin1234@126.com>
2020-06-07 09:58:22 -0700

    LocalStorage for Auth

--- React/projects/burger-basics/src/containers/BurgerBuildre/BurgerBuilder.js
+++ React/projects/burger-basics/src/containers/BurgerBuildre/BurgerBuilder.js
@@ -48,7 +48,8 @@ class BurgerBuilder extends Component {
     purchaseHandler = () => {
         if (this.props.isAuthenticated) {
             this.setState({ purchasing: true });
-        }else{
+        } else {
+            this.props.onSetAuthRedirectPath('/checkout');
             this.props.history.push('/auth')
         }
 
@@ -152,7 +153,8 @@ const mapDispatchToProps = dispatch => {
         onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
         onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
         onInitIngredients: () => dispatch(actions.initIngredients()),
-        onInitPurchase: () => dispatch(actions.purchaseInit())
+        onInitPurchase: () => dispatch(actions.purchaseInit()),
+        onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
     };
 };
 
--- React/projects/burger-basics/src/store/actions/auth.js
+++ React/projects/burger-basics/src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import * as actionTypes from './actionsTypes';
 import axios from 'axios';
+import logo from '../../components/Logo/Logo';
 
 export const authStart = () => {
     return {
@@ -38,6 +39,10 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken)
+                localStorage.setItem('expirationDate', expirationDate)
+                localStorage.setItem('userId', response.data.localId)
                 dispatch(authSuccess(response.data.idToken, response.data.localId))
                 dispatch(checkAuthTimeout(response.data.expiresIn))
             })
@@ -50,6 +55,9 @@ export const auth = (email, password, isSignup) => {
 }
 
 export const logout = () => {
+    localStorage.removeItem("token")
+    localStorage.removeItem("expirationDate")
+    localStorage.removeItem('userId')
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -60,5 +68,30 @@ export const checkAuthTimeout = (expirationTime) => {
         setTimeout(() => {
             dispatch(logout());
         }, expirationTime * 1000)
+    }
+}
+
+export const setAuthRedirectPath = (path) => {
+    return {
+        type: actionTypes.SET_AUTH_REDIRECT_PATH,
+        path: path
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token')
+        if (!token) {
+            dispatch(logout())
+        } else {
+            const expirationDate = new Date(localStorage.getItem("expirationDate"))
+            if (expirationDate > new Date()) {
+                const userId = localStorage.getItem('userId')
+                dispatch(authSuccess(token, userId))
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000))
+            } else {
+                dispatch(logout())
+            }
+        }
     }
 }
