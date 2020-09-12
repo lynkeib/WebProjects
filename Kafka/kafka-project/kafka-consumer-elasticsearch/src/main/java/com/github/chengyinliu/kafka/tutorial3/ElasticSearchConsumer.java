d5fb128753610e20a87553a17c3d36e7eb9f5858
lynkeib <liuchengyin1234@126.com>
2020-09-12 15:24:33 -0700

    Making SyncCommit for offset for consumer to make sure we have "at leace once" read from kafka

--- Kafka/kafka-project/kafka-consumer-elasticsearch/src/main/java/com/github/chengyinliu/kafka/tutorial3/ElasticSearchConsumer.java
+++ Kafka/kafka-project/kafka-consumer-elasticsearch/src/main/java/com/github/chengyinliu/kafka/tutorial3/ElasticSearchConsumer.java
@@ -69,6 +69,8 @@ public class ElasticSearchConsumer {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
 
         // create consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
@@ -84,6 +86,9 @@ public class ElasticSearchConsumer {
         KafkaConsumer<String, String> consumer = createConsumer("twitter_tweets");
         while(true){
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            logger.info("Received " + records.count() + " records");
+
             for(ConsumerRecord<String, String> record : records){
 //                logger.info("Key: " + record.key() + ", Value: " + record.value());
 //                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
@@ -100,10 +105,18 @@ public class ElasticSearchConsumer {
                 IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
                 logger.info(indexResponse.getId());
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+            }
+            logger.info("Committing offsets ... ");
+            consumer.commitSync();
+            logger.info("Offsets have been committed... ");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
 //        client.close();
