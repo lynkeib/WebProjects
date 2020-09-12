3f8ad072bb3192d6da317960605b3c50d62282b9
lynkeib <liuchengyin1234@126.com>
2020-09-11 23:13:46 -0700

    Setting twitter client

--- Kafka/kafka-project/src/main/java/com/github/chengyinliu/kafka/test/tutorial2/TwitterProducer.java
+++ Kafka/kafka-project/src/main/java/com/github/chengyinliu/kafka/test/tutorial2/TwitterProducer.java
@@ -1,12 +1,17 @@
 package com.github.chengyinliu.kafka.test.tutorial2;
 
 import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
 import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -17,15 +22,77 @@ import java.util.ResourceBundle;
 import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class TwitterProducer {
+
+    static Logger logger = LoggerFactory.getLogger(TwitterProducer.class);
 
     public TwitterProducer() {
 
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-//        new TwitterProducer().run();
+    public static void main(String[] args) {
+        new TwitterProducer().run();
+    }
+
+    public void run() {
+
+        logger.info("Setup");
+
+        /* Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
+        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+
+        // create a twitter client
+        Client client = createTweeterClient(msgQueue);
+
+        // Attempts to establish a connection.
+        client.connect();
+
+        // create a kafka producer
+
+        // loop to send tweets to kafka
+        while (!client.isDone()) {
+            String msg = null;
+            try {
+                msg = msgQueue.poll(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                client.stop();
+            }
+            if (msg != null) {
+                logger.info(msg);
+            }
+        }
+        logger.info("End of the application");
+    }
+
+    private Client createTweeterClient(BlockingQueue<String> msgQueue) {
+        Properties prop = getProperties();
+
+        /* Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
+        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+        List<String> terms = Lists.newArrayList("kafka");
+        hosebirdEndpoint.trackTerms(terms);
+
+        // These secrets should be read from a config.properties file
+        Authentication hosebirdAuth = new OAuth1(
+                prop.getProperty("ConsumerKey"),
+                prop.getProperty("ConsumerSecret"),
+                prop.getProperty("AccessToken"),
+                prop.getProperty("AccessSecret"));
+
+        ClientBuilder builder = new ClientBuilder()
+                .name("Hosebird-Client-01")                              // optional: mainly for the logs
+                .hosts(hosebirdHosts)
+                .authentication(hosebirdAuth)
+                .endpoint(hosebirdEndpoint)
+                .processor(new StringDelimitedProcessor(msgQueue));
+        return builder.build();
+    }
+
+    private Properties getProperties() {
         String f = "config.properties";
         Properties props = new Properties();
         try {
@@ -33,31 +100,6 @@ public class TwitterProducer {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(props.getProperty("test1"));
-    }
-
-    public void run() {
-        // create a twitter client
-
-        // create a kafka producer
-
-        // loop to send tweets to kafka
-    }
-
-    public void createTweeterCClient(){
-        /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
-        BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
-
-        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
-        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
-        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
-// Optional: set up some followings and track terms
-//        List<Long> followings = Lists.newArrayList(1234L, 566788L);
-        List<String> terms = Lists.newArrayList("kafka");
-//        hosebirdEndpoint.followings(followings);
-        hosebirdEndpoint.trackTerms(terms);
-
-// These secrets should be read from a config.properties file
-        Authentication hosebirdAuth = new OAuth1("consumerKey", "consumerSecret", "token", "secret");
+        return props;
     }
 }
