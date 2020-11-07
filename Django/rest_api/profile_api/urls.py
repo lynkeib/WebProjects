ff816411b4602fb9b4e44f66e0caeadd43217fe4
lynkeib <liuchengyin1234@126.com>
2020-11-06 20:33:11 -0800

    Update

--- Django/rest_api/profile_api/urls.py
+++ Django/rest_api/profile_api/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(prefix="hello-viewsset", viewset=views.HelloViewSet, basename="hello-viewset")
 router.register(prefix="profile", viewset=views.UserProfileViewSet)
+router.register(prefix="feed", viewset=views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path("hello-view/", views.HelloApiView.as_view()),
