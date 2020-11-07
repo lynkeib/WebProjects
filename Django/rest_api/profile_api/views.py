24e264a1e9fedc6b101e0deccfdd725767b5d7f9
lynkeib <liuchengyin1234@126.com>
2020-11-06 19:51:30 -0800

    Update

--- Django/rest_api/profile_api/views.py
+++ Django/rest_api/profile_api/views.py
@@ -3,9 +3,12 @@ from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profile_api import serializers
 from profile_api import models
+from profile_api import permissions
 
 
 # Create your views here.
@@ -88,3 +91,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name", "email",)
