bef80524ea7cd61f4f30b5a148cebda9c412f711
lynkeib <liuchengyin1234@126.com>
2020-11-06 20:01:45 -0800

    Update

--- Django/rest_api/profile_api/urls.py
+++ Django/rest_api/profile_api/urls.py
@@ -8,5 +8,6 @@ router.register(prefix="profile", viewset=views.UserProfileViewSet)
 
 urlpatterns = [
     path("hello-view/", views.HelloApiView.as_view()),
-    path("", include(router.urls))
+    path("login/", views.UserLoginApiView.as_view()),
+    path("", include(router.urls)),
 ]
--- Django/rest_api/profile_api/views.py
+++ Django/rest_api/profile_api/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profile_api import serializers
 from profile_api import models
@@ -97,3 +99,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name", "email",)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
